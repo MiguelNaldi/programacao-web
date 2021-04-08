9be4530eac358d3aa3fd203fcb5278d97f05b152
LuanDiass <luan.diass@hotmail.com>
2021-04-08 01:30:19 -0300

    Teste com o fundo do site Original

--- login.js
+++ login.js
@@ -3,10 +3,11 @@ function logar(){
     var email = document.getElementById("email");
     var senha = document.getElementById("senha");
 
-    //console.log(email.value + senha.value); 
-    if (email == "admin@admin.com" && senha == "admin"){
+    console.log(email.value + senha.value); 
+    if (email.value == "admin@admin.com" && senha.value == "admin"){
         localStorage.setItem("acesso", true);
         window.location.href = "index.html"; 
+        alert("Login Bem sucedido !!")
     
     } else {
         alert("Usuário ou Senha Inválidos !!");
