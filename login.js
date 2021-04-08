3e35e8f13ece803e5d65e478a2c7a568ef3a64fe
LuanDiass <luan.diass@hotmail.com>
2021-04-08 00:19:10 -0300

    Login.js Testes

--- login.js
+++ login.js
@@ -1,1 +1,14 @@
-alert('Voce ganhou 1 milhão de bitcoins')
+//alert('Voce ganhou 1 milhão de bitcoins')
+function logar(){
+    var email = document.getElementById("Email");
+    var senha = document.getElementById("Senha");
+
+    if (email.value == "luanzito@master.com" && senha.value == "PaiTaOn"){
+        localStorage.setItem(acesso, true);
+
+        window.location.href = "inde.html";
+    
+    } else {
+        alert("Usuário ou Senha Inválidos !!");
+    }
+}
