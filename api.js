3f9efe6e656b98fa33b2ed227b1b4c0c0e4284ff
LuanDiass <luan.diass@hotmail.com>
2021-04-15 08:52:50 -0300

    Testes de Retorno de Filme no .JS

--- api.js
+++ api.js
@@ -1,33 +1,60 @@
-document.getElementById("formulario").addEventListener("submit", pesquisarFilme);
+const $form = document.querySelector('[data-js="formButton"]')
+const $formInput = document.querySelector('[data-js="formInput"]')
 
-function pesquisarFilme(e){
-    alert("Funcionando Função Pesquisar filme");
-    e.preventDefault();
+$form.addEventListener("click", event => pesquisarFilme(event));
+
+function pesquisarFilme(event){
+    event.preventDefault();
+
+    buscarFilmes($formInput.value)
+    $formInput.value = ""
 }
 
-function buscarFilmes(filmePesquisa){
-    axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=79e3fb1d' + filmePesquisa)
-    .then(funcion (response)){
-        console.log(response);
-        var filmes = response.data.Search;
-        var mostrarFilmes = '';
+const buscarFilmes = async filmePesquisa => {
+    try{
+        const response = await axios.get(`http://www.omdbapi.com/?s=${filmePesquisa}&apikey=79e3fb1d`)
 
-        for (var i = 0; i < filmes.lenght; i++){
-            mostrarFilmes += 
-        }
+        const { Search } = response.data
+
+        const filmes = [...Search]
+        showFilmes(filmes)
+
+    }catch(error){
+        alert("Filme não encontrado")
+        console.log(error)
     }
-
-    document.getElementById("filmes").innerHTML = mostrarFilmes;
-    .catch(function (error) {
-        console.log(error);
-        
-    });
-
 }
+function showFilmes(filmes){
+    const $card = document.querySelector('[data-js="card"]')
 
-function filmeDetalhes(id){
-    sessionStorage.setItem(filmeID, id);
-    window.location = "detalhes.html";
-    return false;
+    // Retorna apenas 1 filme
+
+        const $innerCard = document.createElement("div");
+        $innerCard.setAttribute("class", "card")
+        $innerCard.innerHTML = `
+            <div style="z-index: 90">
+                <h1>${filmes[0].Title} - ${filmes[0].Year}</h1>
+                <img src=${filmes[0].Poster} alt="${filmes[0].Title}"/>
+            </div>
+        `
     
+        $card.appendChild($innerCard)
+
+    
+    
+    // Retorna até 10 filmes
+  
+    // filmes.forEach((film) => {
+    //     const $innerCard = document.createElement("div");
+    //     $innerCard.setAttribute("class", "card")
+    //     $innerCard.innerHTML = `
+    //         <div style="z-index: 90">
+    //             <h1>${film.Title} - ${film.Year}</h1>
+    //             <img src=${film.Poster} alt="${film.Title}"/>
+    //         </div>
+    //     `
+    
+    //     $card.appendChild($innerCard)
+    // })
+
 }
