315c163c71035a09a2bfcb7eb2c916e4a55feba5
LuanDiass <luan.diass@hotmail.com>
2021-04-26 20:20:14 -0300

    "Usuario ou senha inválidos" Funcionando

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
     const $inputEmail = document.querySelector('[data-js="inputEmail"]')
     const $inputPassword = document.querySelector('[data-js="inputPassword"]')
-    const $warningText = querySelector('[data-js="warningText"]')
+    const $warningText = document.querySelector('[data-js="warningText"]')
 
     const LogIn = async () => {
         const data =  {
@@ -10,18 +10,15 @@
 
         try {
             const response = await axios.post("https://reqres.in/api/login", data)
-
             const { token } = response.data
 
             localStorage.setItem("@token:netflix", token)
 
             window.location.href = "api.html"
             
-        }catch (err) {
-            
-            $warningText.setAtribute("style", "display: block")
+        } catch (err) {
+            $warningText.setAttribute("style", "display: block")
 
-           // alert("Usuário ou senha inválidos")
         }
     }
  
