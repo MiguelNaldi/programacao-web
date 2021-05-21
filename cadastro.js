ee5514aae8b8be6df57d0169c703d50a36c5454f
miguelnaldi <miguel.naldi@outlook.com>
2021-05-20 21:20:49 -0300

    commit on master

--- cadastro.js
+++ cadastro.js
@@ -1,36 +1,34 @@
-const $inputEmail = document.querySelector('[data-js="inputEmail"]')
-const $inputPassword = document.querySelector('[data-js="inputPassword"]')
-const $inputSobrenome = document.querySelector('[data-js="inputSobrenome"]')
-const $inputNome = document.querySelector('[data-js="inputNome"]')
-const $inputuserName = document.querySelector('[data-js="inputuserName"]')
-const $inputAdm = document.querySelector('[data-js="inputAdm"]')
+const $inputEmail = document.querySelector('[data-js="inputEmail"]');
+const $inputPassword = document.querySelector('[data-js="inputPassword"]');
+const $inputSobrenome = document.querySelector('[data-js="inputSobrenome"]');
+const $inputNome = document.querySelector('[data-js="inputNome"]');
+const $inputuserName = document.querySelector('[data-js="inputuserName"]');
+const $inputAdm = document.querySelector('[data-js="inputAdm"]');
 
-const $warningText = document.querySelector('[data-js="warningText"]')
-
+const $warningText = document.querySelector('[data-js="warningText"]');
 
 const Cadastro = async () => {
-    const data =  {
-        "email":  $inputEmail.value,
-        "password": $inputPassword.value,
-        "username": $inputuserName.value,
-        "firstName": $inputNome.value,
-        "lastName": $inputSobrenome.value,
-        "administrador": $inputAdm.checked
+  const data = {
+    email: $inputEmail.value,
+    password: $inputPassword.value,
+    username: $inputuserName.value,
+    firstName: $inputNome.value,
+    lastName: $inputSobrenome.value,
+    administrator: $inputAdm.checked,
+  };
+  console.log(data);
 
+  try {
+    const response = await axios.post(
+      "http://localhost:3001/api/register",
+      data
+    );
+    const token = `Bearer ${response.data.token}`;
 
-        }
-        console.log(data)
+    localStorage.setItem("@token:netflix", token);
 
-    try {
-        const response = await axios.post("http://localhost:3001/api/register", data)
-        const token = `Bearer ${response.data.token}`
-
-        localStorage.setItem("@token:netflix", token)
-
-        window.location.href = "login.html"
-        
-    } catch (err) {
-        $warningText.setAttribute("style", "display: block")
-
-    }
-}
+    window.location.href = "login.html";
+  } catch (err) {
+    $warningText.setAttribute("style", "display: block");
+  }
+};
